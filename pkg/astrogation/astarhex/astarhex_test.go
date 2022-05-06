3ec6e4334c34d115e4fcfc06204c043368091444
Galdoba <galdoba@mail.ru>
2022-05-06 18:41:59 +1000

    astar ripof

--- pkg/astrogation/astarhex/astarhex_test.go
+++ pkg/astrogation/astarhex/astarhex_test.go
@@ -1,9 +1,348 @@
 package astarhex
 
-/*
+import (
+	"testing"
+)
 
-   ___
-  /   \
-  \___/
+func TestAstar_H(t *testing.T) {
+	nodeA := Node{X: 0, Y: 0}
+	nodeB := Node{X: 1, Y: 0}
+	nodeC := Node{X: 1, Y: 1}
+	nodeD := Node{X: 2, Y: 5}
 
-*/
+	// invalid grid
+	_, err := New(Config{GridWidth: 1, GridHeight: 1})
+	if err == nil {
+		t.Fatal("there should be a grid min error", err)
+	}
+
+	// setup a 6x6 grid
+	a, err := New(Config{GridWidth: 6, GridHeight: 6})
+	if err != nil {
+		t.Fatal("there should be no error", err)
+	}
+
+	if a.H(nodeA, nodeB) != 1 {
+		t.Error("should be 1")
+	}
+	if a.H(nodeA, nodeA) != 0 {
+		t.Error("should be 0")
+	}
+	if a.H(nodeA, nodeC) != 2 {
+		t.Error("should be 2")
+	}
+	if a.H(nodeB, nodeD) != 6 {
+		t.Error("should be 6")
+	}
+}
+
+func TestGetNeighborNodes(t *testing.T) {
+
+	// setup a 4x4 grid
+	a, err := New(Config{GridWidth: 4, GridHeight: 4})
+	if err != nil {
+		t.Fatal("there should be no error", err)
+	}
+	wantList := NewList()
+	invalidList := NewList()
+
+	defer func() {
+		wantList.Clear()
+		invalidList.Clear()
+	}()
+
+	node := Node{X: 2, Y: 2}
+
+	validNeighbors := []Node{
+		{X: 2, Y: 3}, // up
+		{X: 2, Y: 1}, // down
+		{X: 1, Y: 2}, // left
+		{X: 3, Y: 2}, // right
+	}
+
+	invalidNeighbors := []Node{
+		{X: 0, Y: 3},
+		{X: 0, Y: 2},
+		{X: 0, Y: 0},
+		{X: 3, Y: 3},
+		{X: 1, Y: 1},
+		{X: 3, Y: 1},
+		{X: 2, Y: 0},
+		{X: 9, Y: 9},
+	}
+
+	wantList.Add(validNeighbors...)
+	invalidList.Add(invalidNeighbors...)
+
+	for index, neighbor := range a.GetNeighborNodes(node) {
+		if wantList.Contains(neighbor) {
+			wantList.Remove(neighbor)
+		}
+		if invalidList.Contains(neighbor) {
+			t.Error("this neighbor node should be invalid: ", node)
+		}
+		if index > len(validNeighbors) {
+			t.Error("more neighbors found as expected")
+		}
+	}
+
+	if !wantList.IsEmpty() {
+		t.Error("not all expected neighbors found: ", wantList.All())
+	}
+
+}
+
+func TestGetNeighborNodesWithInvalidNodes(t *testing.T) {
+
+	wantList := NewList()
+	obstacleList := NewList()
+	defer func() {
+		wantList.Clear()
+		obstacleList.Clear()
+	}()
+
+	node := Node{X: 2, Y: 2}
+
+	validNeighbors := []Node{
+		{X: 2, Y: 3}, // up
+		{X: 3, Y: 2}, // right
+	}
+
+	obstacleNodes := []Node{
+		{X: 2, Y: 1}, // down is not accessible
+		{X: 1, Y: 2}, // left is not accessible
+	}
+
+	wantList.Add(validNeighbors...)
+	obstacleList.Add(obstacleNodes...)
+
+	// setup a 4x4 grid
+	a, err := New(Config{GridWidth: 4, GridHeight: 4, InvalidNodes: obstacleNodes})
+	if err != nil {
+		t.Fatal("there should be no error", err)
+	}
+
+	for index, neighbor := range a.GetNeighborNodes(node) {
+		if wantList.Contains(neighbor) {
+			wantList.Remove(neighbor)
+		}
+		if obstacleList.Contains(neighbor) {
+			t.Error("this neighbor node should be invalid: ", node)
+		}
+		if index > len(validNeighbors) {
+			t.Error("more neighbors found as expected")
+		}
+	}
+
+	if !wantList.IsEmpty() {
+		t.Error("not all expected neighbors found: ", wantList.All())
+	}
+
+}
+
+func TestAstar_FindPathA(t *testing.T) {
+
+	// [ ] [ ] [ ] [ ] [ ]   S: StartNode
+	// [ ] [O] [ ] [E] [ ]   E: EndNode
+	// [ ] [ ] [O] [P] [ ]   O: ObstacleNode
+	// [ ] [S] [P] [P] [ ]   P: Valid Path
+	// [ ] [ ] [ ] [ ] [ ]
+
+	startNode := Node{X: 1, Y: 1}
+	endNode := Node{X: 3, Y: 3}
+	obstacleNodes := []Node{
+		{X: 1, Y: 3},
+		{X: 2, Y: 2},
+	}
+
+	pathNodesToFind := []Node{
+		{X: 2, Y: 1},
+		{X: 3, Y: 1},
+		{X: 3, Y: 2},
+		{X: 3, Y: 3},
+	}
+
+	pathList := NewList()
+
+	defer func() {
+		pathList.Clear()
+	}()
+
+	pathList.Add(pathNodesToFind...)
+
+	// setup a 5x5 grid
+	a, err := New(Config{GridWidth: 5, GridHeight: 5, InvalidNodes: obstacleNodes})
+	if err != nil {
+		t.Fatal("there should be no error", err)
+	}
+	foundPath, err := a.FindPath(startNode, endNode)
+	if err != nil {
+		t.Error("there should be a path", err)
+	}
+
+	for index, pathNode := range foundPath {
+		if pathList.Contains(pathNode) {
+			pathList.Remove(pathNode)
+		}
+		if index > len(pathNodesToFind) {
+			t.Error("more path nodes found as expected")
+		}
+	}
+
+	if !pathList.IsEmpty() {
+		t.Error("not all expected path nodes found: ", pathList.All())
+	}
+}
+
+func TestAstar_FindPathB(t *testing.T) {
+
+	// [ ] [ ] [ ] [ ] [ ]   S: StartNode
+	// [ ] [E] [P] [ ] [ ]   E: EndNode
+	// [ ] [O] [P] [O] [O]   O: ObstacleNode
+	// [ ] [O] [S] [ ] [ ]   P: Valid Path
+	// [ ] [ ] [ ] [ ] [ ]
+
+	startNode := Node{X: 2, Y: 1}
+	endNode := Node{X: 1, Y: 3}
+	obstacleNodes := []Node{
+		{X: 3, Y: 2},
+		{X: 4, Y: 2},
+		{X: 1, Y: 1},
+		{X: 1, Y: 2},
+	}
+
+	pathNodesToFind := []Node{
+		{X: 2, Y: 2},
+		{X: 2, Y: 3},
+		{X: 1, Y: 3},
+	}
+
+	pathList := NewList()
+
+	defer func() {
+		pathList.Clear()
+	}()
+
+	pathList.Add(pathNodesToFind...)
+
+	// setup a 5x5 grid
+	a, err := New(Config{GridWidth: 5, GridHeight: 5, InvalidNodes: obstacleNodes})
+	if err != nil {
+		t.Fatal("there should be no error", err)
+	}
+	foundPath, err := a.FindPath(startNode, endNode)
+	if err != nil {
+		t.Error("there should be a path", err)
+	}
+
+	for index, pathNode := range foundPath {
+		if pathList.Contains(pathNode) {
+			pathList.Remove(pathNode)
+		}
+		if index > len(pathNodesToFind) {
+			t.Error("more path nodes found as expected")
+		}
+	}
+
+	if !pathList.IsEmpty() {
+		t.Error("not all expected path nodes found: ", pathList.All())
+	}
+}
+
+func TestAstar_FindPathC(t *testing.T) {
+
+	// [ ] [ ] [ ] [ ] [ ]   S: StartNode
+	// [ ] [ ] [ ] [ ] [ ]   E: EndNode
+	// [ ] [ ] [ ] [ ] [ ]   O: ObstacleNode
+	// [P] [E] [ ] [ ] [ ]   P: Valid Path
+	// [S] [P] [ ] [ ] [ ]
+
+	startNode := Node{X: 0, Y: 0}
+	endNode := Node{X: 1, Y: 1}
+
+	// way A
+	pathA := []Node{
+		{X: 0, Y: 1},
+		{X: 1, Y: 1},
+	}
+
+	// way B
+	pathB := []Node{
+		{X: 1, Y: 0},
+		{X: 1, Y: 1},
+	}
+
+	pathAList := NewList()
+	pathBList := NewList()
+
+	defer func() {
+		pathAList.Clear()
+		pathBList.Clear()
+	}()
+
+	pathAList.Add(pathA...)
+	pathBList.Add(pathB...)
+
+	// setup a 5x5 grid
+	a, err := New(Config{GridWidth: 5, GridHeight: 5})
+	if err != nil {
+		t.Fatal("there should be no error", err)
+	}
+	foundPath, err := a.FindPath(startNode, endNode)
+	if err != nil {
+		t.Error("there should be a path", err)
+	}
+
+	for index, pathNode := range foundPath {
+		// check for way A
+		if pathAList.Contains(pathNode) {
+			pathAList.Remove(pathNode)
+		}
+		// check for way B
+		if pathBList.Contains(pathNode) {
+			pathBList.Remove(pathNode)
+		}
+		if index >= len(pathA)+len(pathB) {
+			t.Error("we get 2 found path ways...")
+		}
+	}
+
+	// one list must be empty here
+	if !pathAList.IsEmpty() == !pathBList.IsEmpty() {
+		t.Error("not all expected path nodes found!")
+	}
+}
+
+func TestAstar_FindPathNoPath(t *testing.T) {
+
+	// [ ] [ ] [ ] [ ] [E]   S: StartNode
+	// [ ] [ ] [ ] [ ] [ ]   E: EndNode
+	// [O] [O] [O] [O] [O]   O: ObstacleNode
+	// [ ] [ ] [ ] [ ] [ ]   P: Valid Path
+	// [S] [ ] [ ] [ ] [ ]
+
+	startNode := Node{X: 0, Y: 0}
+	endNode := Node{X: 4, Y: 4}
+	obstacleNodes := []Node{
+		{X: 0, Y: 2},
+		{X: 1, Y: 2},
+		{X: 2, Y: 2},
+		{X: 3, Y: 2},
+		{X: 4, Y: 2},
+	}
+
+	// setup a 5x5 grid
+	a, err := New(Config{GridWidth: 5, GridHeight: 5, InvalidNodes: obstacleNodes})
+	if err != nil {
+		t.Fatal("there should be no error", err)
+	}
+	foundPath, err := a.FindPath(startNode, endNode)
+	if err == nil {
+		t.Error("there should be no path", err)
+	}
+
+	if len(foundPath) > 0 {
+		t.Error("there should be no foundPath", foundPath)
+	}
+
+}
--- pkg/astrogation/astarhex/list.go
+++ pkg/astrogation/astarhex/list.go
@@ -1,26 +1,83 @@
 package astarhex
 
-//List - список нодов
+import "errors"
+
+// List represents a list of nodes
 type List struct {
 	nodes []Node
 }
 
-//NewList - создает список
+// NewList creates a new list
 func NewList() *List {
 	return &List{}
 }
 
-//Add - добавляет 1 или более нод в список
+// Add one or more nodes to the list
 func (l *List) Add(nodes ...Node) {
 	l.nodes = append(l.nodes, nodes...)
 }
 
-//Add - возвращает весь список нодов
+// All returns the full list of nodes
 func (l *List) All() []Node {
 	return l.nodes
 }
 
-//Remove - убирает нод из списка. если его нет, то ничего не делает
-func (l *List) Remove(n Node) {
+// Remove a node from the list
+// if the node is not found we do nothing
+func (l *List) Remove(removeNode Node) {
+	index := l.GetIndex(removeNode)
+	if index >= 0 {
+		l.nodes = append(l.nodes[:index], l.nodes[index+1:]...)
+	}
+}
 
+// GetIndex returns the index of the node in the list
+// if the node is not found the return value is -1
+func (l *List) GetIndex(searchNode Node) int {
+	for index, node := range l.nodes {
+		if node.X == searchNode.X && node.Y == searchNode.Y {
+			return index
+		}
+	}
+	return -1
+}
+
+// Contains check if a node is in the list
+func (l *List) Contains(searchNode Node) bool {
+	return l.GetIndex(searchNode) >= 0
+}
+
+// IsEmpty returns if the nodes list has nodes or not
+func (l *List) IsEmpty() bool {
+	return len(l.nodes) == 0
+}
+
+// Clear removes all nodes from the list
+func (l *List) Clear() {
+	l.nodes = []Node{}
+}
+
+// GetIndexOfMinF returns the index of the nodes list
+// with the smallest node.F value
+//
+// if no node is found it returns -1
+func (l *List) GetIndexOfMinF() int {
+	lastNode := Node{}
+	lastNodeIndex := -1
+	for index, node := range l.nodes {
+		if lastNodeIndex == -1 || node.f < lastNode.f {
+			lastNode = node
+			lastNodeIndex = index
+		}
+	}
+	return lastNodeIndex
+}
+
+// GetMinFNode returns the node with the smallest node.F value
+func (l *List) GetMinFNode() (Node, error) {
+	minFIndex := l.GetIndexOfMinF()
+	if minFIndex == -1 {
+		return Node{}, errors.New("no node found")
+	}
+	return l.nodes[minFIndex], nil
 }
