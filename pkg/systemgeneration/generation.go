3496191202df5ef8a705c9b6958aba53a9c074d0
Galdoba <galdoba@mail.ru>
2022-08-26 20:05:37 +1000

    step 07

--- pkg/systemgeneration/generation.go
+++ pkg/systemgeneration/generation.go
@@ -1,33 +1,47 @@
 package systemgeneration
 
 import (
+	"fmt"
+
 	"github.com/Galdoba/TravellerTools/internal/dice"
 )
 
 const (
-	DefaultValue = iota
-	SubsectorEmpty
-	SubsectorScattered
-	SubsectorDispersed
-	SubsectorAverage
-	SubsectorCrowded
-	SubsectorDense
-	ObjectNONE
-	ObjectStar
-	ObjectBrownDwarf
-	ObjectRoguePlanet
-	ObjectRogueGasGigant
-	ObjectNeutronStar
-	ObjectNebula
-	ObjectBlackHole
+	DefaultValue             = iota
+	SubsectorEmpty           = "Empty"
+	SubsectorScattered       = "Scattered"
+	SubsectorDispersed       = "Dispersed"
+	SubsectorAverage         = "Average"
+	SubsectorCrowded         = "Crowded"
+	SubsectorDense           = "Dense"
+	ObjectUNDEFINED          = "UNDEFINED"
+	ObjectNONE               = "NONE"
+	ObjectPRESENT            = "PRESENT"
+	ObjectStar               = "Star"
+	ObjectBrownDwarf         = "BrownDwarf"
+	ObjectRoguePlanet        = "RoguePlanet"
+	ObjectRogueGasGigant     = "RogueGasGigant"
+	ObjectNeutronStar        = "NeutronStar"
+	ObjectNebula             = "Nebula"
+	ObjectBlackHole          = "BlackHole"
+	StarSystemRealistic      = "Realistic"
+	StarSystemSemiRealistic  = "Semi-Realistic"
+	StarSystemFantastic      = "Fantastic"
+	StarPopulationUNKNOWN    = "Unknown"
+	StarPopulationSolo       = "Single Star System"
+	StarPopulationBinary     = "Binary Star System"
+	StarPopulationTrinary    = "Trinary Star System"
+	StarPopulationQuatenary  = "Quatenary Star System"
+	StarPopulationQuintenary = "Quintenary Star System"
 )
 
 type GenerationState struct {
-	Dice        *dice.Dicepool
-	SystemName  string
-	CurrentStep int
-	NextStep    int
-	System      *StarSystem
+	Dice          *dice.Dicepool
+	SystemName    string
+	ConcludedStep int
+	NextStep      int
+	System        *StarSystem
+	vocal         bool
 }
 
 type Generator interface {
@@ -37,30 +51,389 @@ func NewGenerator(name string) (*GenerationState, error) {
 	gs := GenerationState{}
 	gs.Dice = dice.New().SetSeed(name)
 	gs.Dice.Vocal()
+	gs.vocal = true
 	gs.SystemName = name
+	gs.debug(fmt.Sprintf("SystemName set as %v", name))
 	gs.NextStep = 1
-	sts, _ := gs.NewStarSystem(SubsectorDense)
+	sts, err := gs.NewStarSystem(SubsectorAverage, StarSystemRealistic)
+	if err != nil {
+		return &gs, err
+	}
 	gs.System = sts
 	return &gs, nil
 }
 
-func (gs *GenerationState) NewStarSystem(stsType int) (*StarSystem, error) {
+func (gs *GenerationState) NewStarSystem(stsType, ssType string) (*StarSystem, error) {
 	ss := StarSystem{}
 	ss.subsectorType = stsType
+	ss.starSystemType = ssType
+	ss.starPopulation = StarPopulationUNKNOWN
+	ss.ObjectType = ObjectUNDEFINED
+	gs.debug(fmt.Sprintf("ObjectType set as %v\n", ObjectUNDEFINED))
+	gs.debug(fmt.Sprintf("starSystemType set as %v\n", ssType))
 	return &ss, nil
 }
 
-func (gs *GenerationState) Step01() {
-
-}
-
 type StarSystem struct {
-	subsectorType int
-	ObjectType    int
+	subsectorType  string
+	starSystemType string
+	starPopulation string
+	ObjectType     string
+	Stars          []star
 }
 
-func (gs *GenerationState) RollD100() {
-	gs.Dice.Roll("1d100")
+type star struct {
+	class     string
+	num       int
+	size      string
+	generated bool
+}
+
+func (gs *GenerationState) GenerateData() error {
+	err := fmt.Errorf("initial error")
+	err = nil
+	for gs.ConcludedStep < 20 {
+		switch gs.NextStep {
+		default:
+			err = fmt.Errorf("gs.NextStep = %v unimplemented", gs.NextStep)
+		case 1:
+			err = gs.Step01()
+		case 2:
+			err = gs.Step02()
+		case 3:
+			err = gs.Step03()
+		case 4:
+			err = gs.Step04()
+		case 5:
+			err = gs.Step05()
+		case 6:
+			err = gs.Step06()
+		case 20:
+			err = gs.Step20()
+			if err == nil {
+				return nil
+			}
+		}
+		gs.trackStatus()
+		if err != nil {
+			return fmt.Errorf("GenerateData returned err=%v\n concluded Step = %v\n next step = %v", err.Error(), gs.ConcludedStep, gs.NextStep)
+		}
+	}
+	return fmt.Errorf("unresolved generation\n concluded Step = %v\n next step = %v", gs.ConcludedStep, gs.NextStep)
+}
+
+func (gs *GenerationState) Step01() error {
+	fmt.Println("START Step 01")
+	if gs.NextStep != 1 {
+		fmt.Errorf("not actual step")
+	}
+	tn := 0
+	switch gs.System.subsectorType {
+	case SubsectorEmpty:
+		tn = 5
+	case SubsectorScattered:
+		tn = 20
+	case SubsectorDispersed:
+		tn = 35
+	case SubsectorAverage:
+		tn = 50
+	case SubsectorCrowded:
+		tn = 60
+	case SubsectorDense:
+		tn = 75
+	}
+	gs.System.ObjectType = ObjectNONE
+	gs.debug("ObjectType set as NONE")
+	presenceRoll := gs.Dice.Roll("1d100").Sum()
+	if presenceRoll <= tn {
+		gs.System.ObjectType = ObjectPRESENT
+		gs.debug("ObjectType set as PRESENT")
+	}
+	switch gs.System.ObjectType {
+	default:
+		return fmt.Errorf("system ObjectType is invalid")
+	case ObjectNONE:
+		gs.debug("ObjectType Is not in the hex: END GENERATION")
+		gs.NextStep = 20
+	case ObjectPRESENT:
+		gs.NextStep = 2
+	}
+	gs.ConcludedStep = 1
+	fmt.Println("END Step 01")
+	return nil
+}
+
+func (gs *GenerationState) Step02() error {
+	fmt.Println("START Step 02")
+	if gs.NextStep != 2 {
+		return fmt.Errorf("not actual step")
+	}
+	typeRoll := gs.Dice.Roll("1d100").Sum()
+	switch {
+	case typeRoll <= 80:
+		gs.System.ObjectType = ObjectStar
+		gs.NextStep = 4
+	case typeRoll <= 88:
+		gs.System.ObjectType = ObjectBrownDwarf
+		gs.NextStep = 3
+	case typeRoll <= 94:
+		gs.System.ObjectType = ObjectRoguePlanet
+		gs.NextStep = 15
+	case typeRoll <= 97:
+		gs.System.ObjectType = ObjectRogueGasGigant
+		gs.NextStep = 13
+	case typeRoll <= 98:
+		gs.System.ObjectType = ObjectNeutronStar
+		gs.NextStep = 18
+	case typeRoll <= 99:
+		gs.System.ObjectType = ObjectNebula
+		gs.NextStep = 18
+	case typeRoll <= 100:
+		gs.System.ObjectType = ObjectBlackHole
+		gs.NextStep = 20
+	}
+	gs.debug(fmt.Sprintf("gs.System.ObjectType set as %v", gs.System.ObjectType))
+	gs.ConcludedStep = 2
+	switch gs.NextStep {
+	case 4, 3, 15, 13, 18, 20:
+	default:
+		return fmt.Errorf("gs.NextStep imposible")
+	}
+	fmt.Println("END Step 02")
+	return nil
+}
+
+func (gs *GenerationState) Step03() error {
+	fmt.Println("START Step 03")
+	if gs.NextStep != 3 {
+		return fmt.Errorf("not actual step")
+	}
+	starType := ""
+	dwarfTypeRoll := gs.Dice.Roll("1d100").Sum()
+	switch {
+	case dwarfTypeRoll <= 50:
+		starType = "L"
+	case dwarfTypeRoll <= 75:
+		starType = "T"
+	case dwarfTypeRoll <= 100:
+		starType = "Y"
+	}
+	str := star{class: starType, num: -1, size: ""}
+	gs.System.Stars = append(gs.System.Stars, str)
+	gs.debug("starType is " + starType)
+	gs.NextStep = 5
+	gs.debug(fmt.Sprintf("gs.System.ObjectType set as %v", gs.System.ObjectType))
+	gs.ConcludedStep = 3
+	switch gs.NextStep {
+	case 5:
+	default:
+		return fmt.Errorf("gs.NextStep imposible")
+	}
+	fmt.Println("END Step 03")
+	return nil
+}
+
+func (gs *GenerationState) Step04() error {
+	fmt.Println("START Step 04")
+	if gs.NextStep != 4 {
+		return fmt.Errorf("not actual step")
+	}
+	starType := ""
+	starTypeRoll := gs.Dice.Roll("1d100").Sum()
+	tn := []int{}
+	switch gs.System.starSystemType {
+	default:
+		return fmt.Errorf("unknown gs.System.starSystemType (%v)", gs.System.starSystemType)
+	case StarSystemRealistic:
+		tn = []int{80, 88, 94, 97, 98, 99, 100}
+	case StarSystemSemiRealistic:
+		tn = []int{50, 77, 90, 97, 98, 99, 100}
+	case StarSystemFantastic:
+		tn = []int{25, 50, 75, 97, 98, 99, 100}
+	}
+	switch {
+	case starTypeRoll <= tn[0]:
+		starType = "M"
+	case starTypeRoll <= tn[1]:
+		starType = "K"
+	case starTypeRoll <= tn[2]:
+		starType = "G"
+	case starTypeRoll <= tn[3]:
+		starType = "F"
+	case starTypeRoll <= tn[4]:
+		starType = "A"
+	case starTypeRoll <= tn[5]:
+		starType = "B"
+	case starTypeRoll <= tn[6]:
+		starType = "O"
+	}
+	str := star{class: starType, num: -1, size: ""}
+	gs.System.Stars = append(gs.System.Stars, str)
+	gs.debug("starType is " + starType)
+	gs.NextStep = 5
+	gs.debug(fmt.Sprintf("gs.System.ObjectType set as %v", gs.System.ObjectType))
+	gs.ConcludedStep = 4
+	switch gs.NextStep {
+	case 5:
+	default:
+		return fmt.Errorf("gs.NextStep imposible")
+	}
+	fmt.Println("END Step 04")
+	return nil
+}
+
+func (gs *GenerationState) Step05() error {
+	fmt.Println("START Step 05")
+	if gs.NextStep != 5 {
+		return fmt.Errorf("not actual step")
+	}
+	numRoll := gs.Dice.Roll("1d10").Sum()
+	for i, star := range gs.System.Stars {
+		if star.num != -1 {
+			continue
+		}
+		gs.System.Stars[i].num += numRoll
+		gs.debug(fmt.Sprintf("gs.System.Stars[%v].num set as %v", i, gs.System.Stars[i].num))
+		gs.ConcludedStep = 5
+		switch gs.System.Stars[i].class {
+		case "O", "B", "A", "F", "G", "K", "M":
+			gs.NextStep = 6
+		case "L", "T", "Y":
+			gs.NextStep = 7
+		}
+	}
+	switch gs.NextStep {
+	case 6, 7:
+	default:
+		return fmt.Errorf("gs.NextStep imposible")
+	}
+	fmt.Println("END Step 05")
+	return nil
+}
+
+func (gs *GenerationState) Step06() error {
+	fmt.Println("START Step 06")
+	if gs.NextStep != 6 {
+		return fmt.Errorf("not actual step")
+	}
+	lumRoll1 := gs.Dice.Roll("1d100").Sum()
+	lumRoll2 := gs.Dice.Roll("1d10").Sum()
+	for i, star := range gs.System.Stars {
+		if star.size != "" {
+			continue
+		}
+		switch {
+		case lumRoll1 <= 90:
+			gs.System.Stars[i].size = "V"
+		case lumRoll1 <= 94:
+			gs.System.Stars[i].size = "IV"
+		case lumRoll1 <= 96:
+			gs.System.Stars[i].size = "D"
+		case lumRoll1 <= 99:
+			gs.System.Stars[i].size = "III"
+		case lumRoll1 <= 100:
+			gs.System.Stars[i].size = ""
+		}
+		if gs.System.Stars[i].size == "D" {
+			gs.System.Stars[i].class = ""
+		}
+		if gs.System.Stars[i].size != "" {
+			continue
+		}
+		switch {
+		case lumRoll2 <= 4:
+			gs.System.Stars[i].size = "II"
+		case lumRoll2 <= 6:
+			gs.System.Stars[i].size = "VI"
+		case lumRoll2 <= 8:
+			gs.System.Stars[i].size = "Ia"
+		case lumRoll2 <= 10:
+			gs.System.Stars[i].size = "Ib"
+		}
+		switch gs.System.Stars[i].size {
+		case "O", "B", "A", "F":
+			if gs.System.Stars[i].size == "VI" {
+				gs.System.Stars[i].size = "V"
+			}
+		}
+	}
+	gs.ConcludedStep = 6
+	gs.NextStep = 7
+	switch gs.NextStep {
+	case 7:
+	default:
+		return fmt.Errorf("gs.NextStep imposible")
+	}
+	fmt.Println("END Step 06")
+	return nil
+}
+
+func (gs *GenerationState) Step07() error {
+	fmt.Println("START Step 07")
+	if gs.NextStep != 7 {
+		return fmt.Errorf("not actual step")
+	}
+	switch gs.System.starPopulation {
+	default:
+		return fmt.Errorf("star population unexpected")
+	case StarPopulationSolo, StarPopulationBinary, StarPopulationTrinary, StarPopulationQuatenary, StarPopulationQuintenary:
+		gs.ConcludedStep = 7
+		for i := range gs.System.Stars {
+			if gs.System.Stars[i].num == -1 || gs.System.Stars[i].class == "" {
+				gs.NextStep = 4
+			}
+		}
+	case "":
+		tn := []int{}
+		switch gs.System.Stars[0].class {
+		case "O", "B", "A":
+			tn = []int{10, 90, 98, 99, 100}
+		case "F", "G", "K":
+			tn = []int{45, 99, 100, 599, 999}
+		case "M", "L", "T", "Y", "":
+			tn = []int{69, 98, 100, 200, 300}
+		}
+		strComposRoll := gs.Dice.Roll("1d100").Sum()
+		switch {
+		case strComposRoll <= tn[0]:
+			gs.System.starPopulation = StarPopulationSolo
+		case strComposRoll <= tn[1]:
+			gs.System.starPopulation = StarPopulationBinary
+		case strComposRoll <= tn[2]:
+			gs.System.starPopulation = StarPopulationTrinary
+		case strComposRoll <= tn[3]:
+			gs.System.starPopulation = StarPopulationQuatenary
+		case strComposRoll <= tn[4]:
+			gs.System.starPopulation = StarPopulationQuintenary
+		}
+	}
+	switch gs.NextStep {
+	case 4, 8:
+	default:
+		return fmt.Errorf("gs.NextStep imposible")
+	}
+	fmt.Println("END Step 07")
+	return nil
+}
+
+func (gs *GenerationState) Step20() error {
+	fmt.Println("START Step 20")
+	if gs.NextStep != 20 {
+		return fmt.Errorf("not actual step")
+	}
+	gs.NextStep = 99
+	fmt.Println("END Step 20")
+	return nil
+}
+
+func (gs *GenerationState) debug(str string) {
+	if gs.vocal {
+		fmt.Println(str)
+	}
+}
+
+func (gs *GenerationState) trackStatus() {
+	fmt.Printf("generation steps %v/%v\n", gs.ConcludedStep, gs.NextStep)
 }
 
 /*
--- pkg/systemgeneration/generation_test.go
+++ pkg/systemgeneration/generation_test.go
@@ -1,8 +1,38 @@
 package systemgeneration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGeneration(t *testing.T) {
-	gen, _ := NewGenerator("Sol")
-	gen.Step01()
+	for _, name := range []string{
+		"Sol",
+		"Alpha Centaury",
+		"Deneb",
+		"Antares",
+	} {
+		gen, _ := NewGenerator(name)
+		if gen.NextStep != 1 {
+			t.Errorf("have nextstep=%v (expect 1)", gen.NextStep)
+		}
+		if gen.System == nil {
+			t.Errorf("Star System not generated")
+		}
+		if gen.System.ObjectType != ObjectUNDEFINED && gen.System.ObjectType != ObjectNONE {
+			t.Errorf("Star System object not set\n  have %v (expect %v or %v)", gen.System.ObjectType, ObjectUNDEFINED, ObjectNONE)
+		}
+		if err := gen.GenerateData(); err != nil {
+			t.Errorf("error: %v", err.Error())
+		}
+		fmt.Println(gen.System)
+		// gen.Step01()
+		// gen.trackStatus()
+		// gen.Step02()
+		// gen.trackStatus()
+		fmt.Println(" ")
+		fmt.Println("//////////")
+		fmt.Println(" ")
+	}
+
 }
