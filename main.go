a001b65b930a7c28cfda58e82fc3640ff8c67755
Galdoba <galdoba@mail.ru>
2023-04-28 20:28:42 +1000

    111

--- main.go
+++ main.go
@@ -1,42 +1,34 @@
 package main
 
-import (
-	"image/color"
-	"log"
+// type Game struct{}
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-)
+// func (g *Game) Update() error {
+// 	return nil
+// }
 
-type Game struct{}
+// func (g *Game) Draw(screen *ebiten.Image) {
+// 	ebitenutil.DebugPrint(screen, "Hello, World!")
+// 	im := ebiten.NewImage(50, 50)
+// 	im.Fill(color.Gray{})
+// 	im2 := ebiten.NewImage(500, 500)
+// 	im.DrawImage(im2, &ebiten.DrawImageOptions{
+// 		GeoM:          ebiten.GeoM{},
+// 		ColorM:        ebiten.ColorM{},
+// 		CompositeMode: ebiten.CompositeModeCopy,
+// 		Filter:        ebiten.FilterLinear,
+// 	})
+// }
 
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-	im := ebiten.NewImage(50, 50)
-	im.Fill(color.Gray{})
-	im2 := ebiten.NewImage(500, 500)
-	im.DrawImage(im2, &ebiten.DrawImageOptions{
-		GeoM:          ebiten.GeoM{},
-		ColorM:        ebiten.ColorM{},
-		CompositeMode: ebiten.CompositeModeCopy,
-		Filter:        ebiten.FilterLinear,
-	})
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480
-}
+// func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+// 	return 640, 480
+// }
 
 func main() {
-	ebiten.SetWindowSize(1280, 960)
-	ebiten.SetWindowTitle("Hello, World!")
+	// ebiten.SetWindowSize(1280, 960)
+	// ebiten.SetWindowTitle("Hello, World!")
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
-		log.Fatal(err)
+	// if err := ebiten.RunGame(&Game{}); err != nil {
+	// 	log.Fatal(err)
 
-	}
+	// }
 }
