8b2ab7d47ed3000220e82c67040af1f264bfb970
Galdoba <galdoba@mail.ru>
2024-03-06 17:06:34 +1000

    g

--- hostile/character/career/career.go
+++ hostile/character/career/career.go
@@ -2,90 +2,52 @@ package career
 
 import (
 	"fmt"
-
-	"github.com/Galdoba/TravellerTools/pkg/dice"
 )
 
 const (
-	EnlistedByQualific = "Q"
-	EnlistetByDraft    = "D"
-	///////
-	Android          = "0"
-	CorporateAgent   = "1"
-	CorporateExec    = "2"
-	Colonist         = "3"
-	CommersialSpacer = "4"
-	Marine           = "5"
-	Marshal          = "6"
-	MilitarySpacer   = "7"
-	Physician        = "8"
-	Ranger           = "9"
-	Rogue            = "A"
-	Roughneck        = "B"
-	Scientist        = "C"
-	SurveyScout      = "D"
-	Technitian       = "E"
-	///////
-	PersDevel    = "1"
-	Service      = "2"
-	Specialist   = "3"
-	AdvancedEdu  = "4"
-	ComMilSpacer = "5"
-	Scout        = "6"
-	PhysSkill    = "7"
-	ScienceSkill = "8"
-	TechSkill    = "9"
-	////////
-	SurvivalFailed = "S"
-	MishapEvent    = "M"
-	////////
-	CommisionPassed = "Y"
-	CommisionFailed = "N"
-	////////
-	AdvancementGained = "+"
-	AdvancementMissed = "-"
-	AdvancementHalted = "="
-	////////
-	ReenlistFailed = "N"
-	ReenlistPassed = "Y"
-	ReenlistForsed = "F"
+	Android            = "0"
+	CorporateAgent     = "1"
+	CorporateExec      = "2"
+	Colonist           = "3"
+	CommersialSpacer   = "4"
+	Marine             = "5"
+	Marshal            = "6"
+	MilitarySpacer     = "7"
+	Physician          = "8"
+	Ranger             = "9"
+	Rogue              = "A"
+	Roughneck          = "B"
+	Scientist          = "C"
+	SurveyScout        = "D"
+	Technitian         = "E"
+	CommisionPassed    = true
+	CommisionNotPassed = false
 )
 
-type CareerPath struct {
-	careersFinished int
-	draftUsed       bool
+type Career struct {
+	Name            string
+	code            string
+	CommissionState bool
+	Rank            int
+	TermsCompleted  int
 }
 
-type term struct {
-	careerCode      string
-	skillTable      string
-	skillRoll       string
-	commisionRoll   string
-	advancementRoll string
-	reenlistRoll    string
-}
-
-// 125101
-func Injury(dice *dice.Dicepool) string {
-	code := "I"
-	r := dice.Sroll("1d6")
-	code += fmt.Sprintf("%v", r)
-	switch dice.Sroll("1d6") {
-	case 1:
-		c := dice.Sroll("1d3") - 1
-		for i := 0; i < 3; i++ {
-			if i == c {
-				r2 := dice.Sroll("1d6")
-
-			}
-		}
-
-	case 2:
-	case 3:
-	case 4:
-	case 5:
-	case 6:
-
+func New(code string) (*Career, error) {
+	cr := Career{}
+	switch code {
+	default:
+		return nil, fmt.Errorf("can't create career: unknown code '%v'", code)
+	case Android:
+		return nil, fmt.Errorf("Android career is not implemented")
 	}
-	return code
+	return &cr, nil
+}
+
+type CareerStats struct {
+	Name          string `json:"Career"`
+	Qualification string `json:"Qualification"`
+	Survival      string `json:"Survival"`
+	Commision     string `json:"Commision"`
+	Advance       string `json:"Advance"`
+	ReEnlist      int    `json:"Re-Enlist"`
 }
--- hostile/character/characteristic/characteristic.go
+++ hostile/character/characteristic/characteristic.go
@@ -1,6 +1,11 @@
 package characteristic
 
-import "github.com/Galdoba/TravellerTools/pkg/dice"
+import (
+	"fmt"
+
+	"github.com/Galdoba/TravellerTools/pkg/dice"
+	"github.com/Galdoba/TravellerTools/pkg/ehex"
+)
 
 const (
 	Unknown = iota
@@ -15,6 +20,100 @@ const (
 
 type CharSet struct {
 	chars map[int]int
+}
+
+type Char struct {
+	Name    string //STR/DEX/END/INT/EDU/SOC
+	Type    string //Physical/Mental/Social
+	Maximum ehex.Ehex
+	Current int
+}
+
+func New(code int) (*Char, error) {
+	chr := Char{}
+	switch code {
+	default:
+		return nil, fmt.Errorf("can't create char with code %v", code)
+	case STR:
+		chr.Name = "STR"
+		chr.Type = "Physical"
+	case DEX:
+		chr.Name = "DEX"
+		chr.Type = "Physical"
+	case END:
+		chr.Name = "END"
+		chr.Type = "Physical"
+	case INT:
+		chr.Name = "INT"
+		chr.Type = "Mental"
+	case EDU:
+		chr.Name = "EDU"
+		chr.Type = "Mental"
+	case SOC:
+		chr.Name = "SOC"
+		chr.Type = "Social"
+	}
+	return &chr, nil
+}
+
+func (chr *Char) Roll(dice *dice.Dicepool) {
+	r := dice.Sroll("2d6")
+	chr.Maximum = ehex.New().Set(r)
+	chr.Current = r
+}
+
+func (chr *Char) ChangeMaximumBy(i int) {
+	v := chr.Maximum.Value() + i
+	if v > 15 {
+		v = 15
+	}
+	if v < 0 {
+		v = 0
+	}
+	chr.Maximum = ehex.New().Set(v)
+}
+
+func (chr *Char) Damage(i int) int {
+	chr.Current -= i
+	if chr.Current < 0 {
+		v := -1 * chr.Current
+		chr.Current = 0
+		return v
+	}
+	return 0
+}
+
+func (chr *Char) Heal(i int) {
+	v := chr.Current + i
+	if v > chr.Maximum.Value() {
+		v = chr.Maximum.Value()
+	}
+	chr.Current = v
+}
+
+func (c *Char) Mod() int {
+	return charMod(c.Current)
+}
+
+func charMod(i int) int {
+	switch i {
+	default:
+		if i <= 2 {
+			return -2
+		}
+		if i >= 15 {
+			return 3
+		}
+	case 3, 4, 5:
+		return -1
+	case 6, 7, 8:
+		return 0
+	case 9, 10, 11:
+		return 1
+	case 12, 13, 14:
+		return 2
+	}
+	return -999
 }
 
 func (cs *CharSet) Strength() int {
